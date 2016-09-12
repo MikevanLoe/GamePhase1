bb76ed6e43dd7cb0b9c4a5bf955ff5394f5adccd
MikevanLoe <mikevanloe@gmail.com>
2016-09-12 13:53:37 +0200

    spacing ~mike

--- Test Island/Assets/Scripts/Creatures/StatProfiles/BeastProfile.cs
+++ Test Island/Assets/Scripts/Creatures/StatProfiles/BeastProfile.cs
@@ -3,11 +3,11 @@
 public class BeastProfile : Profile
 {
 	public BeastProfile (GrowthRate spc = GrowthRate.SuperSlow,
-	                        GrowthRate def = GrowthRate.Faster,
-	                        GrowthRate atk = GrowthRate.SuperFast,
-	                        GrowthRate spd = GrowthRate.Average,
-	                        GrowthRate end = GrowthRate.Average,
-	                        GrowthRate exp = GrowthRate.Fast)
+	                     GrowthRate def = GrowthRate.Faster,
+	                     GrowthRate atk = GrowthRate.SuperFast,
+	                     GrowthRate spd = GrowthRate.Average,
+	                     GrowthRate end = GrowthRate.Average,
+	                     GrowthRate exp = GrowthRate.Fast)
 		: base (spc, def, atk, spd, end, exp)
 	{
 
