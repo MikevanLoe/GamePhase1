48d7d0e8860438017e76bb1cb264e22f190ca136
MikevanLoe <mikevanloe@gmail.com>
2016-12-18 19:32:26 +0100

    TP_test 2 new islands minor tweaks ~Mike

---
+++ Test Island/Assets/Scripts/Eiland_TP_Test.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Eiland_TP_Test : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        //Application.LoadLevel("Eiland_grootst");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Eiland_grootst");
+    }
+}
